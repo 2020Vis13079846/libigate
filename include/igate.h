38fdf8afa61a19d6e931849348a0115494236845
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-13 18:52:10 +0300

    Update igate.h

--- include/igate.h
+++ include/igate.h
@@ -6,6 +6,12 @@ extern "C" {
 #endif
 
 void show_commands();
+int send_command(char *argv[]);
+void connect();
+void disconnect();
+void reset();
+int shell();
+void init();
 
 #ifdef __cplusplus
 }
