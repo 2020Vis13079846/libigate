af9755e1cce4a1727bafe3f90b621cf2122925e1
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-10-13 19:07:02 +0300

    Update igate.c

--- src/igate.c
+++ src/igate.c
@@ -7,9 +7,6 @@
 #include <libusb-1.0/libusb.h>
 #include <usb.h>
 
-#include <readline/readline.h>
-#include <readline/history.h>
-
 /*
 BootROM -> LLB -> iBoot -> SEP -> Kernel -> Applications
 */
@@ -21,15 +18,9 @@ BootROM -> LLB -> iBoot -> SEP -> Kernel -> Applications
 #define DFU_MODE        (int)0x1222 // DFU Mode
 #define BUF_SIZE        (int)0x10000
 
-#define CMD_LOG	".orbit_history" /* history file for readline/history.h */
+#define CMD_LOG	".igate_history" /* history file for readline/history.h */
 
 struct libusb_device_handle *device = NULL; /* initalize device variable with NULL value */
-
-void show_commands(bool is_global)
-{
-    if (is_global) printf("Usage: orbit [option] <argumants>\n\n  -s, --shell  Interact with iDevice.\n");
-    else printf("  reboot  Reboot iDevice.\n  go <address>  Jump to the specified memory address.\n help  Show available commands.\n");
-}
 
 int send_command(char* command) 
 {
@@ -66,78 +57,11 @@ void disconnect()
     }
 }
 
-// DANGEROUS: Do not use on new iPhones (only iPhone Original - iPhone 3GS)
-
 void reset() 
 {
     if (device != NULL) {
 	libusb_reset_device(device);
     }
-}
-
-int shell() 
-{
-    if (libusb_set_configuration(device, 1) < 0) {	
-	return 101; // setting consiguration error
-    }
-	
-    if (libusb_claim_interface(device, 1) < 0) {
-	return 102; // interface error
-    }
-	
-    if (libusb_set_interface_alt_setting(device, 1, 1) < 0) {
-	return 103; // alt interface error
-    }
-	
-    char* buffer = malloc(BUF_SIZE);
-    if (buffer == NULL) {
-	return 104; // memory allocation error
-    }
-	
-    FILE* fd = NULL;
-    for (;;) {	
-	int bytes = 0;
-	memset(buffer, 0, BUF_SIZE);
-	libusb_bulk_transfer(device, 0x81, buffer, BUF_SIZE, &bytes, 500);
-		
-	if (bytes > 0) {
-	    int i;
-            for(i = 0; i < bytes; ++i) {
-		fprintf(stdout, "%c", buffer[i]);
-		if (fd) fprintf(fd, "%c", buffer[i]);
-	    }
-	}
-	char *command = readline("(console)> ");
-		
-	if (command != NULL) {
-	    add_history(command);
-	    if (fd) fprintf(fd, ">%s\n", command);
-	    char* cmd = &command[0];
-            size_t length = strlen(command);
-
-     	    if (length >= 0x200) {
-		printf("Failed to execute command!\n");
-    	    }
-	
-    	    if (! libusb_control_transfer(device, 0x40, 0, 0, 0, command, (length + 1), 1000)) {
-		printf("Failed to execute command!\n");
-    	    }
-				
-	    char* action = strtok(strdup(command), " ");
-	    if (! strcmp(action, "getenv")) {
-	        char response[0x200];
-		libusb_control_transfer(device, 0xC0, 0, 0, 0, response, 0x200, 1000);
-		printf("%s\r\n", response);		
-	    }
-				
-	    if (! strcmp(action, "reboot") || ! strcmp(action, "poweroff")) return 105; // terminated
-	}	
-        free(command);
-    }
-    free(buffer);
-    if (fd) fclose(fd);
-    libusb_release_interface(device, 1);
-    return 100; // okk
 }
 
 void init() 
